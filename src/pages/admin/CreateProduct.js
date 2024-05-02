48d0d921caad1cfb507b3512fdfbe9242e52a345
kkumardeshmukh <kdkrantikumar@gmail.com>
2024-05-02 12:27:08 +0530

    product creation successsull

--- src/pages/admin/CreateProduct.js
+++ src/pages/admin/CreateProduct.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import Layout from '../../components/Layouts/Layout'
 import Adminmenu from '../../components/Layouts/Adminmenu'
@@ -8,8 +9,10 @@ import Adminmenu from '../../components/Layouts/Adminmenu'
 
 const CreateProduct = () => {
 
+    const navigate = useNavigate()
+
     const [categories, setCategories] = useState([])
-    const [category, setCategory] = useState([])
+    const [category, setCategory] = useState("")
     const [name, setName] = useState("")
     const [description, setDescription] = useState("")
     const [price, setPrice] = useState("")
@@ -36,6 +39,39 @@ const CreateProduct = () => {
         getAllCategory()
     }, []);
 
+    const handleCreateProduct = async (e) => {
+        e.preventDefault()
+
+        try {
+
+            const productData = new FormData()
+            productData.append("name", name)
+            productData.append("description", description)
+            productData.append("price", price)
+            productData.append("quantity", quantity)
+            productData.append("category", category)
+            productData.append("photo", photo)
+            productData.append("shipping", shipping)
+
+            console.log(category, shipping)
+
+            const { data } = await axios.post('http://localhost:8080/api/v1/product/create-product', productData)
+
+            console.log(data)
+
+            if (data.success) {
+                alert('product created successfully')
+                navigate('/dashboard/admin/products')
+
+            } else {
+                alert('something went wrong')
+            }
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
 
 
@@ -51,19 +87,18 @@ const CreateProduct = () => {
                             PRODUCTS
                         </div>
                         <div>
+                            {/* <form > */}
                             <div>
                                 <select
-                                    //add a search option on dropdown
-                                    bordered={false}
-                                    placeholder="Select a category of product" size='large'
                                     className="form-select my-4 p-3 w-75"
-                                    onChange={(value) => { setCategory(value) }}>
-                                    <option>Select Category</option>
+                                    onChange={(e) => setCategory(e.target.value)}>
+                                    <option value={null}>Select Category</option>
 
                                     {categories.map(c => (
-                                        <option key={c._id} value={c.name}>{c.name}</option>
+                                        <option key={c._id} value={c._id}>{c.name}</option>
                                     ))}
                                 </select>
+                                <p>{category}</p>
                             </div>
 
                             <div>
@@ -99,18 +134,39 @@ const CreateProduct = () => {
                                     onChange={(e) => { setName(e.target.value) }}
                                 />
                                 <textarea
-                                    placeholder="Product Name"
+                                    placeholder="Product Description"
                                     value={description}
                                     className="form-control my-3"
-                                    onChange={(e) => { setName(e.target.value) }}
+                                    onChange={(e) => { setDescription(e.target.value) }}
                                 />
-                                <input type="text"
-                                    placeholder="Product Name"
-                                    value={description}
+                                <input type="number"
+                                    placeholder="product Price"
+                                    value={price}
                                     className="form-control my-3"
-                                    onChange={(e) => { setName(e.target.value) }}
+                                    onChange={(e) => { setPrice(e.target.value) }}
                                 />
+                                <input type="number"
+                                    placeholder="product Quantity"
+                                    value={quantity}
+                                    className="form-control my-3"
+                                    onChange={(e) => { setQuantity(e.target.value) }}
+                                />
+
                             </div>
+                            <div>
+                                <select
+                                    className="form-select my-4 p-3 w-75"
+                                    onChange={(e) => { setShipping(e.target.value) }}>
+                                    <option>Shipping</option>
+                                    <option value={false}>No</option>
+                                    <option value={true}>Yes</option>
+                                </select>
+                                <p>{shipping}</p>
+                            </div>
+                            <div className="mb-5">
+                                <button className="btn btn-primary w-75" type="submit" onClick={handleCreateProduct}>Create Product</button>
+                            </div>
+                            {/* </form> */}
 
 
 
